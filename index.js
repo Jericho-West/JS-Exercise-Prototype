9238e057210f027f58b50be1252fe453857fd484
cnelsonw <cnelsonw7@gmail.com>
2022-01-19 17:22:30 -0500

    c

--- index.js
+++ index.js
@@ -73,8 +73,9 @@ Person.prototype.toString = function () {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-  
+function Car(model, milesPerGallon) {
+  this.model = model,
+  this.milesPerGallon = milesPerGallon
 }
 
 
